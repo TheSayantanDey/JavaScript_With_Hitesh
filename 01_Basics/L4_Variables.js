0831febbb7615aee1203a8659ee573bc27afb97d
Sayantan Dey <107317508+TheSayantanDey@users.noreply.github.com>
2024-07-07 18:48:20 +0000

    L4 and L5

---
+++ 01_Basics/L4_Variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345; // Can't be updated
+let accountHolderName = "Sayantan"; // Can be updated
+var password = "abcde" // Can be updated
+address = "kolkata" // Can be updated
+let nominee; // holds undefined
+
+console.table([accountId, accountHolderName, password, address, nominee]);
---
+++ 01_Basics/L5_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all JS code as newer version -- good practice
+
+let name = "saya"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+
+console.log(typeof state); // undefined
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
