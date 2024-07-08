4c512ce45dc10333135f08317b49631e00895be1
Sayantan Dey <107317508+TheSayantanDey@users.noreply.github.com>
2024-07-08 17:46:15 +0000

    L7, L8, L9 done

---
+++ 01_Basics/L9_Datatypes.js
@@ -0,0 +1,48 @@
+//  Primitive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+// console.log(typeof score)  // number
+
+const scoreValue = 100.3
+// console.log(typeof scoreValue)  // number
+
+const isLoggedIn = false
+// console.log(typeof isLoggedIn)  // boolean
+
+const outsideTemp = null
+// console.log(typeof outsideTemp)  // object
+
+let userEmail;
+// console.log(typeof userEmail)  // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id === anotherId);
+// console.log(typeof id)  // symbol
+
+const bigNumber = 3456543576654356754n
+// console.log(typeof bigNumber)  // bigint
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+// console.log(typeof heros); //object
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+// console.log(typeof myObj)  // object
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof myFunction); // function
+
+
+//Docs at :-  https://262.ecma-international.org/5.1/#sec-11.4.3
